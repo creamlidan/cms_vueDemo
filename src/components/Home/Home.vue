d5d465c329cb010dd1e98ba4e864f5ccddcde92a
lidan <creamlidan@163.com>
2017-07-09 23:56:45 +0800

    轮播图组件的抽离

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,11 +1,7 @@
 <template>
 	<div class="templ">
 		<!-- banner -->
-		<mt-swipe :auto="40000" class="swipe_wrap">
-			<mt-swipe-item v-for="(item,index) in list" :key="index">
-				<a href="##" :style="item.backgroundImg" class="swipe_img"></a>
-			</mt-swipe-item>
-		</mt-swipe>
+		<bannerSlide :imageList ="list"></bannerSlide>
 		<!-- 导航区 -->
 		<div class="mui-content">
 			<ul class="mui-table-view mui-grid-view mui-grid-9">
@@ -50,6 +46,7 @@
 	</div>
 </template>
 <script>
+	import bannerSlide from '../common/bannerSlide.vue'
 	export default{
 		data(){
 			return{
@@ -65,66 +62,51 @@
 				this.$http.get(url).then(
 					function(res){
 						this.list = JSON.parse(res.bodyText);
-						console.log(this.list)
 					},
 					function(err){
 						console.log(err)
 					})
 			}
-		}
+		},
+		components:{
+    		bannerSlide
+    	}
 	}
 </script>
 <style scoped>
-		.mui-content{
-			background: none;
-		}
-		.swipe_wrap,.mint-swipe-items-wrap{
-			height:2.6rem;
-		}
-		.swipe_img{
-			display: block;
-			width: 100%;
-			height:100%;
-			background-position:center;
-			background-size:100%;
-			background-repeat: no-repeat;
-		}
-		.swipe_wrap .mint-swipe-indicator{
-			opactiy:0.5 !important;
-		}
-		.swipe_wrap .mint-swipe-indicator.is-active{
-			background: #26a2ff !important;
-		}
-		.mui-grid-view.mui-grid-9,.mui-grid-view.mui-grid-9 .mui-table-view-cell{
-			border:none;
-			background: none;
-		}
-		.mui-icon-home::before,.mui-icon-email::before,.mui-icon-chatbubble::before,.mui-icon-location::before,.mui-icon-search::before,.mui-icon-phone::before{
-			content: "";
-		}
-		.mui-icon-home,.mui-icon-email,.mui-icon-chatbubble,.mui-icon-location,.mui-icon-search,.mui-icon-phone{
-			width: 41px;
-			height: 41px;
-			background-repeat: no-repeat;
-			background-position: center;
-			background-size: 100%;
-		}
-		.mui-icon-home{
-			background-image: url('../../statics/images/icon/menu10.png');
-		}
-		.mui-icon-email{
-			background-image: url('../../statics/images/icon/menu3.png');
-		}
-		.mui-icon-chatbubble{
-			background-image: url('../../statics/images/icon/menu4.png');
-		}
-		.mui-icon-location{
-			background-image: url('../../statics/images/icon/menu5.png');
-		}
-		.mui-icon-search{
-			background-image: url('../../statics/images/icon/menu6.png');
-		}
-		.mui-icon-phone{
-			background-image: url('../../statics/images/icon/menu9.png');
-		}
+	.mui-content{
+		background: none;
+	}
+	.mui-grid-view.mui-grid-9,.mui-grid-view.mui-grid-9 .mui-table-view-cell{
+		border:none;
+		background: none;
+	}
+	.mui-icon-home::before,.mui-icon-email::before,.mui-icon-chatbubble::before,.mui-icon-location::before,.mui-icon-search::before,.mui-icon-phone::before{
+		content: "";
+	}
+	.mui-icon-home,.mui-icon-email,.mui-icon-chatbubble,.mui-icon-location,.mui-icon-search,.mui-icon-phone{
+		width: 41px;
+		height: 41px;
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: 100%;
+	}
+	.mui-icon-home{
+		background-image: url('../../statics/images/icon/menu10.png');
+	}
+	.mui-icon-email{
+		background-image: url('../../statics/images/icon/menu3.png');
+	}
+	.mui-icon-chatbubble{
+		background-image: url('../../statics/images/icon/menu4.png');
+	}
+	.mui-icon-location{
+		background-image: url('../../statics/images/icon/menu5.png');
+	}
+	.mui-icon-search{
+		background-image: url('../../statics/images/icon/menu6.png');
+	}
+	.mui-icon-phone{
+		background-image: url('../../statics/images/icon/menu9.png');
+	}
 </style>
---
+++ src/components/common/bannerSlide.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="banTempl">
+		<!-- banner -->
+		<mt-swipe :auto="40000" class="swipe_wrap">
+			<mt-swipe-item v-for="(item,index) in imageList" :key="index">
+				<a href="##" :style="item.backgroundImg" class="swipe_img"></a>
+			</mt-swipe-item>
+		</mt-swipe>
+	</div>
+</template>
+<script>
+	export default{
+		data(){
+			return{
+				
+			}
+		},props:['imageList']
+	}
+</script>
+<style scoped>
+		.mui-content{
+			background: none;
+		}
+		.swipe_wrap,.mint-swipe-items-wrap{
+			height:2.6rem;
+		}
+		.swipe_img{
+			display: block;
+			width: 100%;
+			height:100%;
+			background-position:center;
+			background-size:100%;
+			background-repeat: no-repeat;
+		}
+		.swipe_wrap .mint-swipe-indicator{
+			opactiy:0.5 !important;
+		}
+		.swipe_wrap .mint-swipe-indicator.is-active{
+			background: #26a2ff !important;
+		}
+</style>
